00e39157217cad2928c32c30dde818abe94bf7c1
Anne-Sophie <annesogelinas6@gmail.com>
2024-02-07 13:33:19 -0500

    GestionMedicament

---
+++ app/src/main/java/com/example/projetintp2_android/GestionMedicament.java
@@ -0,0 +1,14 @@
+package com.example.projetintp2_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GestionMedicament extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gestion_medicament);
+    }
+}
--- app/src/main/java/com/example/projetintp2_android/MainActivity.java
+++ app/src/main/java/com/example/projetintp2_android/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.projetintp2_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btConnexion;
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_gestion_medicament);
+
+        /*btConnexion = findViewById(R.id.btConnexion);
+
+        btConnexion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(context, GestionMedicament.class);
+                startActivity(intent);
+            }
+        });*/
     }
 }
