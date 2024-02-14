fabdc46666319dd0e975aa6b7b3982c5c654bf47
Anne-Sophie <annesogelinas6@gmail.com>
2024-02-14 13:02:48 -0500

    gg

--- app/src/main/java/com/example/projetintp2_android/MainActivity.java
+++ app/src/main/java/com/example/projetintp2_android/MainActivity.java
@@ -10,22 +10,11 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btConnexion;
-    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gestion_medicament);
 
-        /*btConnexion = findViewById(R.id.btConnexion);
-
-        btConnexion.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(context, GestionMedicament.class);
-                startActivity(intent);
-            }
-        });*/
     }
 }
