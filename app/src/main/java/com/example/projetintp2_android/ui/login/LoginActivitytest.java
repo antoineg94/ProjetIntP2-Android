0ae0e6c12fcf2f74e913d02f24d1ea5ab789b2ac
Francine <fmatho@yahoo.com>
2024-02-19 08:24:28 -0500

    creation page testconnexion

---
+++ app/src/main/java/com/example/projetintp2_android/ui/login/LoginActivitytest.java
@@ -0,0 +1,22 @@
+package com.example.projetintp2_android.ui.login;
+
+import android.os.Bundle;
+import android.widget.EditText;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.projetintp2_android.R;
+
+public class LoginActivitytest extends AppCompatActivity {
+    EditText etusername,etpassword;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logintest);
+
+        
+    }
+}
+
