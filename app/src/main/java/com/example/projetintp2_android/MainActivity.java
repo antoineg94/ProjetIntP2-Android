2d76e0d6df0be6aeba9d845c389530e1802d4815
francine265 <117289117+francine265@users.noreply.github.com>
2024-02-19 16:15:37 -0500

    erreurs a corriger creationcompte

--- app/src/main/java/com/example/projetintp2_android/MainActivity.java
+++ app/src/main/java/com/example/projetintp2_android/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.projetintp2_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+
+import com.example.projetintp2_android.ui.login.LoginActivitytest;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, LoginActivitytest.class);
+                startActivity(intent);
+            }
+        },3000);
     }
 }
--- app/src/main/java/com/example/projetintp2_android/ui/login/LoginActivitytest.java
+++ app/src/main/java/com/example/projetintp2_android/ui/login/LoginActivitytest.java
@@ -1,7 +1,13 @@
 package com.example.projetintp2_android.ui.login;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,14 +15,80 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.projetintp2_android.R;
 
 public class LoginActivitytest extends AppCompatActivity {
-    EditText etusername,etpassword;
+
+    private EditText editTextEmail, editTextPassword;
+    private Button buttonLogin;
+    private TextView textViewForgotPassword, textViewCreateAccount;
+
+    private String email,password;
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logintest);
 
-        
+        editTextEmail = findViewById(R.id.editTextEmail);
+        editTextPassword = findViewById(R.id.editTextPassword);
+        buttonLogin = findViewById(R.id.buttonLogin);
+        textViewForgotPassword = findViewById(R.id.textViewForgotPassword);
+        textViewCreateAccount = findViewById(R.id.textViewCreateAccount);
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkLogin();
+                // Code pour la vérification des informations de connexion et l'authentification
+            }
+        });
+
+        textViewForgotPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Redirection vers la page de réinitialisation du mot de passe
+            }
+        });
+
+        textViewCreateAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Redirection vers la page de création de compte
+            }
+        });
     }
+
+    // methode qui verifie la conxion
+
+    private void checkLogin()
+        {
+            email=editTextEmail.getText().toString();
+            password=editTextPassword.getText().toString();
+            if(email.isEmpty()| password.isEmpty()){
+                alertFail("email et mot de passe requis.");
+            }
+            else {
+                sendLogin();
+            }
+
+    }
+
+    private void sendLogin() {
+        Toast.makeText(this,"Envoyé",Toast.LENGTH_LONG).show();
+    }
+
+    private void alertFail(String s) {
+        new AlertDialog.Builder(this)
+                .setTitle("Echec")
+                .setIcon(R.drawable.ic_loginwarning24)
+                .setMessage(s)
+                .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                })
+                .show();
+    }
+
+
 }
 
