f87bb75315d09c9a36ec3cc062884921c83c904d
anne946 <annesogelinas6@gmail.com>
2024-03-13 15:19:47 -0400

    ggf

--- app/src/main/java/com/example/projetintp2_android/Classes/Retrofit.java
+++ app/src/main/java/com/example/projetintp2_android/Classes/Retrofit.java
@@ -3,8 +3,9 @@ package com.example.projetintp2_android.Classes;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class Retrofit {
-    public static final String BASE_URL = "10.0.2.2";
+//    public static final String BASE_URL = "10.0.2.2";
     private static retrofit2.Retrofit retrofit;
+    public static final String BASE_URL = "https://10.2.43.237:8000/api/";
 
     public static retrofit2.Retrofit getInstance(){
 
